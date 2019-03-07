a24013df9df69957daf87715e762d96461fbcb48
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2019-03-07 11:36:34 +0000

    Split out sending of funds to beneficiary from constructor. Emit event after sending of funds. Add test for approving transfer of funds to beneficiary. Add test for transfer of funds to beneficiary. Add test for reverting when too few approvers.

--- contracts/MultiSignatureWallet.sol
+++ contracts/MultiSignatureWallet.sol
@@ -2,14 +2,18 @@ pragma solidity >=0.4.21 <0.6.0;
 pragma experimental ABIEncoderV2;
 
 contract MultiSignatureWallet {
-    uint minNumberOfApprovers;
+    
+    uint public minNumberOfApprovers;
+    uint public approvalsCount;
+    uint public balance;
 
-    address payable beneficiary;
+    address payable public beneficiary;
     address payable public owner;
 
     mapping (address => bool) approvedBy;
     mapping (address => bool) isApprover;
-    uint approvalsCount;
+
+    event Transferred(address from, uint amount);
 
     constructor(address[] memory _approvers, uint _minNumberOfApprovers, address payable _beneficiary) public payable {
         require(_minNumberOfApprovers <= _approvers.length, "Number of approvers less than required number.");
@@ -24,19 +28,25 @@ contract MultiSignatureWallet {
         }
     }
 
-    function approve() public {
-        require(isApprover[msg.sender], "Not an approver.");
+    function donate() public payable {
+        balance += msg.value;
+    }
 
-        if (!approvedBy[msg.sender]) {
-            approvedBy[msg.sender] = true;
+    function approve(address signer) public {
+        require(isApprover[signer], "Not an approver.");
+
+        if (!approvedBy[signer]) {
+            approvedBy[signer] = true;
             approvalsCount++;
         }
+    }
 
-        if (approvalsCount == minNumberOfApprovers) {
-            beneficiary.transfer(address(this).balance);
-            // Return funds to owner if transfer fails
-            selfdestruct(owner);
-        }
+    function sendToBeneficiary() public payable {
+        require(approvalsCount >= minNumberOfApprovers, "Not approved");
+
+        uint amount = balance; 
+        beneficiary.transfer(amount); 
+        emit Transferred(msg.sender, amount);
     }
 
     function reject() public {
--- test/MultiSignatureWalletTest.js
+++ test/MultiSignatureWalletTest.js
@@ -1,14 +1,82 @@
 const MultiSignatureWallet = artifacts.require("MultiSignatureWallet");
 const truffleAssert = require('truffle-assertions');
+const FINNEY = 1 // 10**15 wei;
 
 contract("MultiSignatureWallet", accounts => {
-
-    const [firstAccount, approver1, approver2, beneficiary] = accounts;
-
+    
     it("sets an owner", async () => {
+        // Arrange
+        const [firstAccount, approver1, approver2, beneficiary] = accounts;
         let approvers = [approver1, approver2];
+        let minNumberOfApprovers = 2;
 
-        let wallet = await MultiSignatureWallet.new(approvers, 2, beneficiary);
+        // Act
+        let wallet = await MultiSignatureWallet.new(approvers, minNumberOfApprovers, beneficiary);
+
+        // Assert
         assert.equal(await wallet.owner.call(), firstAccount);
+    });
+
+    it("approves transfer to beneficiary", async () => {
+        // Arrange
+        const [firstAccount, approver1, approver2, beneficiary] = accounts;
+        let approvers = [firstAccount, approver1, approver2];
+        let minNumberOfApprovers = 2;
+        let wallet = await MultiSignatureWallet.new(approvers, minNumberOfApprovers, beneficiary);
+        await wallet.donate({ from: firstAccount, value: 10 * FINNEY });
+
+        // Act
+        // Get all approvers to approve
+        await wallet.approve(firstAccount);
+        await wallet.approve(approver1);
+        await wallet.approve(approver2);
+
+        // Check
+        let approvalsCount = await wallet.approvalsCount.call();
+        console.log("Approvals count = " + approvalsCount);
+
+        // Assert
+        assert.isTrue(approvalsCount >= minNumberOfApprovers, `Number of approvers ${approvalsCount} is less than required number ${minNumberOfApprovers}.`);
+    });
+
+    it("transfers funds to beneficiary", async () => {
+        // Arrange
+        const [firstAccount, approver1, approver2, beneficiary] = accounts;
+        let approvers = [firstAccount, approver1, approver2];
+        let minNumberOfApprovers = 2;
+        let wallet = await MultiSignatureWallet.new(approvers, minNumberOfApprovers, beneficiary);
+        await wallet.donate({ from: firstAccount, value: 10 * FINNEY });
+        
+        // Get all approvers to approve
+        await wallet.approve(firstAccount);
+        await wallet.approve(approver1);
+        await wallet.approve(approver2);
+        
+        // Act
+        let result = await wallet.sendToBeneficiary();
+
+        // Assert
+        const expectedAmount = 10;
+
+        truffleAssert.eventEmitted(result, 'Transferred', (ev) => {
+            console.log('Event Info');
+            console.log('----------');
+            console.log('Address = ' + ev.from);
+            console.log('Amount = ' + ev.amount);
+      
+            return ev.amount == expectedAmount;
+          }, `Transferred amount should be ${expectedAmount}.`);
+    });
+
+    it("reverts when too few approvers", async () => {
+        // Arrange
+        const [approver1, beneficiary] = accounts;
+        let approvers = [approver1];
+        let minNumberOfApprovers = 2;
+
+        // Act, Assert
+        await truffleAssert.reverts(
+            MultiSignatureWallet.new(approvers, minNumberOfApprovers, beneficiary), 
+            "Number of approvers less than required number.");
     });
 });
